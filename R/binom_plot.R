4ec5482fe215fc8ee043ec063be4b543990c98fd
Mike Seaman <fourthz@gmail.com>
2019-05-22 18:19:58 -0400

    Create documentation for binom_plot

--- R/binom_plot.R
+++ R/binom_plot.R
@@ -1,3 +1,16 @@
+#' Binomial Probability Density Plot
+#'
+#' Plot a simple density plot to illustrate the binomial distribution for a
+#' specified number of trials and a constant probability of success.
+#'
+#' @param n integer
+#' @param p double
+#'
+#' @return plot
+#' @export
+#'
+#' @examples
+#' binom_plot(8, 0.5)
 binom_plot <- function(n, p) {
   x <- 0:n
   prob <- dbinom(x, n, p)
